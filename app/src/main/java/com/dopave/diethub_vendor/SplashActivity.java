fc7a2a1c7504f1c66033aa4bfe9037f1038bb140
Karim Ali <karim.ali1499314@gmail.com>
2020-05-10 16:21:35 +0200

    init splash Activity

--- app/src/main/java/com/dopave/diethub_vendor/SplashActivity.java
+++ app/src/main/java/com/dopave/diethub_vendor/SplashActivity.java
@@ -2,6 +2,7 @@ package com.dopave.diethub_vendor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    //startActivity(new Intent(getApplicationContext(), Login_inActivity.class));
+                    //finish(); // finish(); because This Activity close After move To Next Activity And Next Activity will Be Launcher
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
