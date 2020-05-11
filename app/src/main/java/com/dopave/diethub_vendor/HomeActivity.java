1ddb03a24ba2d1325f1746a6dd8341d32b003ed4
Karim Ali <karim.ali1499314@gmail.com>
2020-05-12 00:46:41 +0200

    desgin Home Activity

--- app/src/main/java/com/dopave/diethub_vendor/HomeActivity.java
+++ app/src/main/java/com/dopave/diethub_vendor/HomeActivity.java
@@ -3,69 +3,73 @@ package com.dopave.diethub_vendor;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Menu;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.annotation.NonNull;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    private AppBarConfiguration mAppBarConfiguration;
-
+    DrawerLayout drawer;
+    String CurrentPage = "nav_Home";
+    String destination = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new
+                ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open
+                , R.string.navigation_drawer_close);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_myAccount, R.id.nav_myOrders)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new Home_Fragment()).commit();
+        getWindow().getDecorView().setSystemUiVisibility
+                (View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR |
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawer.closeDrawer(GravityCompat.START);
         return false;
     }
 
     public void onClick(View view) {
-        Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.action_HomeFragment_to_HomeSecondFragment);
+        if (view.getId() == R.id.nav_Home && !CurrentPage.equals("nav_Home")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new Home_Fragment()).commit();
+            CurrentPage = "nav_Home";
+            drawer.closeDrawer(GravityCompat.START);
+        }else if (view.getId() == R.id.nav_myOrders && !CurrentPage.equals("nav_myOrders")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new OrderFragment()).commit();
+            CurrentPage = "nav_myOrders";
+            drawer.closeDrawer(GravityCompat.START);
+        }else if (view.getId() == R.id.nav_myMeals && !CurrentPage.equals("nav_myMeals")){
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new MealsFragment()).commit();
+            CurrentPage = "nav_myMeals";
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,new Home_Fragment()).commit();
+        //HomeActivity.super.onBackPressed();
+    }
+
 }
