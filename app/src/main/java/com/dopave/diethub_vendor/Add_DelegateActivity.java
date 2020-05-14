37f2c34deed67072b8cb0fde830ed4d87c4e9f00
Karim Ali <karim.ali1499314@gmail.com>
2020-05-14 18:08:37 +0200

    add close keybourd for Setting and add delegate Activities

--- app/src/main/java/com/dopave/diethub_vendor/Add_DelegateActivity.java
+++ app/src/main/java/com/dopave/diethub_vendor/Add_DelegateActivity.java
@@ -1,12 +1,16 @@
 package com.dopave.diethub_vendor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class Add_DelegateActivity extends AppCompatActivity {
-
+    ConstraintLayout Layout_AddDelegate;
+    EditText NameDelegate_Add,EmailDelegate_Add,PhoneNumber_Add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,8 +18,24 @@ public class Add_DelegateActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility
                 (View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR |
                         View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        Layout_AddDelegate = findViewById(R.id.Layout_AddDelegate);
+        NameDelegate_Add = findViewById(R.id.NameDelegate_Add);
+        EmailDelegate_Add = findViewById(R.id.EmailDelegate_Add);
+        PhoneNumber_Add = findViewById(R.id.PhoneNumber_Add);
+        Layout_AddDelegate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeKeyBoard();
+            }
+        });
     }
-
+    private void closeKeyBoard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getWindow().getDecorView().getRootView().getWindowToken(), 0);
+        NameDelegate_Add.clearFocus();
+        EmailDelegate_Add.clearFocus();
+        PhoneNumber_Add.clearFocus();
+    }
     public void BackButton(View view) {
         finish();
     }
--- app/src/main/java/com/dopave/diethub_vendor/SettingsFragment.java
+++ app/src/main/java/com/dopave/diethub_vendor/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.dopave.diethub_vendor;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,11 +10,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.dopave.diethub_vendor.Adapter.AdapterForResImage;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
 
 
 /**
@@ -21,6 +26,8 @@ import java.util.List;
  */
 public class SettingsFragment extends Fragment {
     RecyclerView Recycler;
+    EditText RestaurantsName,About_Res;
+    ConstraintLayout Layout_Setting;
     public SettingsFragment() {
 
     }
@@ -31,10 +38,19 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        RestaurantsName = view.findViewById(R.id.RestaurantsName);
+        About_Res = view.findViewById(R.id.About_Res);
         Recycler = view.findViewById(R.id.Recycler_Res_Icons);
+        Layout_Setting = view.findViewById(R.id.Layout_Setting);
         Recycler.setHasFixedSize(true);
         Recycler.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
         Recycler.setAdapter(new AdapterForResImage(getData(),getContext()));
+        Layout_Setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeKeyBoard();
+            }
+        });
         return view;
     }
 
@@ -47,5 +63,11 @@ public class SettingsFragment extends Fragment {
         list.add("");
         list.add("");
         return list;
+    }
+    private void closeKeyBoard() {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getActivity().getWindow().getDecorView().getRootView().getWindowToken(), 0);
+        RestaurantsName.clearFocus();
+        About_Res.clearFocus();
     }
 }
